48d43edc0b5dbec3b19d9c5fe638bdb08b230ddb
Veton Gacaferi <vetongacaferi95@gmail.com>
2024-02-28 13:55:53 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BuroService';
+  selectedLanguage: string  = '';
+
+  languageList: any[] = [
+    {
+      code: 'en', label:'English',
+    },
+    {
+      code: 'de', label: "Deutsch"
+    },
+    {
+      code: 'sq', label: "Shqip"
+    }
+  ];
+
+  constructor(public translateService: TranslateService) {
+    const defaultLang = this.translateService.defaultLang;
+    
+    console.log('defaultLang:', defaultLang)
+    if(defaultLang)
+    {
+      this.selectedLanguage = this.languageList.find(x => x.code === defaultLang)?.code;
+    }
+  }
+
+
+
+  public onLanguageChange(event: string): void{
+    this.selectedLanguage = event;
+    if(this.selectedLanguage){
+      this.translateService.use(this.selectedLanguage);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: "sq",
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [ HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
